44cc77a6beacbddec8d484e9156d6784dc0019d5
xjj1 <ttodoroff@gmail.com>
2019-02-09 19:06:38 +0200

    First commit

---
+++ main.go
@@ -0,0 +1,37 @@
+// Get a panel as png from grafana
+// click on "share image" / "get direct link rendered image" to get the proper url
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/cavaliercoder/grab"
+	"github.com/pkg/errors"
+)
+
+func main() {
+	url := `http://localhost:3000/render/d-solo/6cE6Zmuiz/hitachi-copy?orgId=1&from=1549413887954&to=1549457721344&panelId=15&width=1000&height=500&tz=Europe%2FKiev`
+	api_key := "eyJrIjoidjBnNG5OMEw2QnBpTW1KRjMzdE00NzdKUjFuMDZjSTAiLCJuIjoia2V5MSIsImlkIjoxfQ=="
+	fname := "image.png"
+	if err := GetImage(fname, url, api_key); err != nil {
+		log.Fatalf("Error getting image : %v", err)
+	}
+	fmt.Println("Download saved to", fname)
+}
+
+func GetImage(fname, url, api_key string) error {
+	os.Remove(fname)
+	client := grab.NewClient()
+	req, err := grab.NewRequest(fname, url)
+	if err != nil {
+		return errors.Wrap(err, "Error creating request")
+	}
+	req.HTTPRequest.Header.Set("Authorization", fmt.Sprintf("Bearer %s", api_key))
+	resp := client.Do(req)
+	if err := resp.Err(); err != nil {
+		return errors.Wrap(err, "Error executing request")
+	}
+	return nil
+}
